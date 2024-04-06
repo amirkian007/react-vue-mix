c3f0a7f062e32f4c87ce4874c05dbd460541f30e
amirkian007 <amircc1200@gmail.com>
2024-04-06 16:30:04 +0330

    init

---
+++ src/components/Connector.tsx
@@ -0,0 +1,13 @@
+import { ReactComponent } from "./ReactComponent";
+import { Root, createRoot } from 'react-dom/client';
+
+
+export class ReactConnector {
+    root:Root;
+    constructor(targetEl:HTMLElement){
+        this.root = createRoot(targetEl);
+    }
+    render(){
+        this.root.render(<ReactComponent/>)
+    }
+}
